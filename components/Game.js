a0728d4d5eb4f3ad1f352b5408756eba31fad35c
mendelB <mendelblesofsky@gmail.com>
2017-06-28 15:37:25 -0400

    Done.

--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,54 @@ const solutions = require('./solutions');
 class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board: [ null, null, null, null, null, null, null, null, null ],
+      turn: 'X'
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault();
+    this.setState({
+      board: [ null, null, null, null, null, null, null, null, null ],
+      turn: 'X'
+    })
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    let board = this.state.board
+    board[i] = this.state.turn
+    this.setState({
+      board: board,
+      turn: this.state.turn === 'X' ? 'O' : 'X'
+    })
   }
 
   getWinner () {
+    let board = this.state.board
+    let winner = solutions.find(sol =>
+      (sol.every((pos, index) => board[pos] === 'X') ||
+      sol.every((pos, index) => board[pos] === 'O'))
+    )
+    if (winner) return board[winner[0]]
   }
 
   isComplete () {
+    return (this.getWinner() || (this.state.board.indexOf(null) === -1))
   }
 
   render () {
     return (
-      <div>
+      <div className='game'>
+        <Board board={this.state.board} onClick={this.handleClick}/>
+        { this.isComplete() ? <Status winner={this.getWinner()}/> : ''}
+        <button className='game__reset' onClick={this.handleReset}>
+          Reset
+        </button>
       </div>
     );
   }
